6fab76c3b8debe9be6138acd7e61372ef374c3e7
OTAHARUNA <radwimps.love867@gmail.com>
2021-10-11 22:10:43 +0900

    DB接続してAPIたたける状態

--- app/app.js
+++ app/app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const sqlite3 = require('sqlite3')
+const dbPath = "app/db/database.sqlite3"
 
+// Get all users
 //getメソッドを投げたときに実行する関数を作成
-app.get('/api/v1/hello', (req, res) => {
-  // レスポンスがjsonメソッドを持っていてこの中に書いたオブジェクトメソッドを返す
-  res.json({"message": "Hello World!"})
+app.get('/api/v1/users', (req, res) => {
+  //connect database
+  const db = new sqlite3.Database(dbPath)
+
+  db.all('SELECT * FROM users', (err, rows) => {
+    // 取得した結果が返ってくる。
+    res.json(rows)
+  })
+
+  db.close()
 })
 // 指定している人がいたらその人のポート番号使ってローカルサーバー立ち上げる
 const port = process.env.PORT || 3000;
